7ca331a594a98063b853de7797bc3531d5650340
Jean-Baptiste Quenot <jbq@caraldi.com>
2015-01-19 15:30:54 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='appdeploy',
-      version='1.0.2',
+      version='1.0.3',
       description="Application deployment tool I use and maintain for years, to be used in a console (uses an ncurses UI)",
       packages=['appdeploy'],
       author='Jean-Baptiste Quenot',
